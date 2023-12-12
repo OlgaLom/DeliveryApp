29f03d6e88b4b8ea2d53137572e5175cbfd304f8
olgaLom <olgamoumtzi@gmail.com>
2023-12-12 22:24:34 +0200

    First init

---
+++ src/main/java/backend_group_5/we_lead_bootcamp/DeliveryAppApplication.java
@@ -0,0 +1,13 @@
+package backend_group_5.we_lead_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend_group_5/we_lead_bootcamp/DeliveryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package backend_group_5.we_lead_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
