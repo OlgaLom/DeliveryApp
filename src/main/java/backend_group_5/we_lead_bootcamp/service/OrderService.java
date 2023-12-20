aa10f3ed874f290455421734e9c2443d521615e7
olgaLom <olgamoumtzi@gmail.com>
2023-12-20 20:43:20 +0200

    Add future note about InitiateOrder Method.

--- src/main/java/backend_group_5/we_lead_bootcamp/service/OrderService.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/service/OrderService.java
@@ -5,6 +5,8 @@ import backend_group_5.we_lead_bootcamp.model.Product;
 
 public interface OrderService extends BaseService<Order, Long>{
     Order InitiateOrder(User user);
+//    We probably need to add the store also
+//    Order InitiateOrder(User user, Store store);
 
     void addItem(Order order, Product product);
 
