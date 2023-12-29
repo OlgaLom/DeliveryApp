08e8b0e1085c650aa353d35fe634796d2cb26eb8
OlgaLom <olgamoumtzi@gmail.com>
2023-12-29 22:44:32 +0200

    Update orderService with code - to be continued

--- src/main/java/backend_group_5/we_lead_bootcamp/service/OrderService.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/service/OrderService.java
@@ -1,21 +1,28 @@
 package backend_group_5.we_lead_bootcamp.service;
 
 import backend_group_5.we_lead_bootcamp.model.Order;
+import backend_group_5.we_lead_bootcamp.model.PaymentMethod;
 import backend_group_5.we_lead_bootcamp.model.Product;
+import backend_group_5.we_lead_bootcamp.model.User;
 
 public interface OrderService extends BaseService<Order, Long>{
-    Order InitiateOrder(User user);
+    Order InitiateOrder(User user, Store store);
 //    We probably need to add the store also
 //    Order InitiateOrder(User user, Store store);
 
-    void addItem(Order order, Product product);
+    void addItem(Order order, Product product, int quantity);
 
-    void UpdateItem(Order order, Product product);
+    void UpdateItem(Order order, Product product, int quantity);
 
-    void RemoveItem(Order order,Product product);
+    void RemoveItem(Order order, Product product);
 
     void SelectAddress(User user);
 
     void SelectPaymentMethod(User user);
+
+    // Ways to create method for Finalize and order.
+//    An order
+    //Order FinalizeOrder(Order order, PaymentMethod p_method,  )
+
 
 }
