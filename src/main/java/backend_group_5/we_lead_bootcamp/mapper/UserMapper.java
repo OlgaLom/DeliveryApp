86122ebfb461abc06ec4c0cf7f84b6acd5883163
GitHub_AnastasiaBam <a.bampasi@gmail.com>
2024-01-11 00:16:42 +0200

    Create account service working

---
+++ src/main/java/backend_group_5/we_lead_bootcamp/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package backend_group_5.we_lead_bootcamp.mapper;
+
+import backend_group_5.we_lead_bootcamp.model.User;
+import backend_group_5.we_lead_bootcamp.transfer.resource.UserResource;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper extends BaseMapper<User, UserResource> {
+}
