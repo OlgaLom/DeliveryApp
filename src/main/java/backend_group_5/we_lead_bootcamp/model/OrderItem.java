c7ac9ca3b550b896b196d06c484db950dd1ec401
olgaLom <olgamoumtzi@gmail.com>
2024-01-18 23:12:11 +0200

    Updated with JPA annotations

--- src/main/java/backend_group_5/we_lead_bootcamp/model/OrderItem.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/model/OrderItem.java
@@ -1,6 +1,8 @@
 package backend_group_5.we_lead_bootcamp.model;
 
 import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,8 +14,26 @@ import java.math.BigDecimal;
 @Setter
 @Builder
 @ToString(callSuper = true)
+
+@Entity
+@Table(name="ORDERITEMS")
+@SequenceGenerator(name = "idGenerator", sequenceName = "ORDER_ITEMS_SEQ", initialValue = 1, allocationSize = 1)
 public class OrderItem extends BaseModel {
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     private Product product;
+
+    // Bidirectional Relationship
+    @ToString.Exclude
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Order order;
+
+    @NotNull
+    @Column(nullable = false)
     private Integer quantity;
+
+    @NotNull
+    @Column(nullable = false)
     private BigDecimal price;
 }
