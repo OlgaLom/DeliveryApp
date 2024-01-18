fd5d054a925691f9177c9ae85e4f4f6c7074a3b0
olgaLom <olgamoumtzi@gmail.com>
2024-01-18 23:12:42 +0200

    Add missing fields to order Resource

--- src/main/java/backend_group_5/we_lead_bootcamp/transfer/resource/OrderResource.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/transfer/resource/OrderResource.java
@@ -1,13 +1,16 @@
 package backend_group_5.we_lead_bootcamp.transfer.resource;
 
+import backend_group_5.we_lead_bootcamp.model.Address;
 import backend_group_5.we_lead_bootcamp.model.OrderStatus;
 import backend_group_5.we_lead_bootcamp.model.PaymentMethod;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sun.nio.sctp.PeerAddressChangeNotification;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.Value;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -17,10 +20,17 @@ import java.util.Set;
 @ToString(callSuper = true)
 public class OrderResource extends BaseResource{
     private UserResource user;
+    private StoreResource store;
     private Set<OrderItemResource> orderItems = new HashSet<>();
-    @JsonFormat(shape = JsonFormat.Shape.STRING)  // We declared in our application yml the default date format.
-    private Date CreateOrderDate;
     private OrderStatus orderStatus;
     private PaymentMethod paymentMethod;
-    private Integer orderTotal;
+    private Address address;
+    private BigDecimal orderTotal;
+    private String orderNumber;
+    private String orderNote;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)  // We declared in our application yml the default date format.
+    private Date createOrderDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private Date updateOrderDate;
+
 }
