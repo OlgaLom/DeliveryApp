dc82bab5b1d2ac29c712d6a79f2d7399f04b8441
olgaLom <olgamoumtzi@gmail.com>
2024-01-21 17:52:29 +0200

    Add new Method toResourceList

--- src/main/java/backend_group_5/we_lead_bootcamp/mapper/OrderMapper.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/mapper/OrderMapper.java
@@ -4,9 +4,12 @@ import backend_group_5.we_lead_bootcamp.model.Order;
 import backend_group_5.we_lead_bootcamp.transfer.resource.OrderResource;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 // @Mapper
 // → Generates implementation code for mapping between Java beans, eliminating the need for boilerplate code.
 @Mapper(componentModel = "spring", config = IgnoreUnmappedMapperConfig.class)
 public interface OrderMapper extends BaseMapper<Order, OrderResource>{
 
+    List<OrderResource> toResourceList(List<Order> userOrders);
 }
