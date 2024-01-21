f10c642c375a953f940bc49cbaf14b4be7c06d03
olgaLom <olgamoumtzi@gmail.com>
2024-01-21 17:52:11 +0200

    Add new Endpoint

--- src/main/java/backend_group_5/we_lead_bootcamp/controller/OrderController.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/controller/OrderController.java
@@ -9,10 +9,12 @@ import backend_group_5.we_lead_bootcamp.transfer.resource.ProductResource;
 import backend_group_5.we_lead_bootcamp.transfer.resource.StoreResource;
 import backend_group_5.we_lead_bootcamp.transfer.resource.UserResource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 // @RestController:
@@ -65,6 +67,7 @@ public class OrderController extends BaseController<Order, OrderResource>{
     //Initiate Order
     // @PostMapping(params = {"user", "store"}) // No need for params we will pass the data to the body
     @RequestMapping("initialize")
+    @PostMapping
     public ResponseEntity<ApiResponse<OrderResource>> createOrder(@RequestBody final UserResource userR, @RequestBody final StoreResource storeR) {
         var user = userMapper.toDomain(userR);
         var store = storeMapper.toDomain(storeR);
@@ -77,6 +80,7 @@ public class OrderController extends BaseController<Order, OrderResource>{
     //Add item
     // @PostMapping(params = {"order","product","quantity"})
     @RequestMapping("/items/add")
+    @PostMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void addItem(@RequestBody final OrderResource orderR, @RequestBody final ProductResource productR, @RequestBody final int quantity){
 
@@ -88,8 +92,9 @@ public class OrderController extends BaseController<Order, OrderResource>{
 
     //Update item
 //    @PutMapping(params = {"order","product","quantity"})
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     @RequestMapping("/items/update")
+    @PutMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateItem(@RequestBody final OrderResource orderR, @RequestBody final ProductResource productR, @RequestBody final int quantity){
         var ord = orderMapper.toDomain(orderR);
         var prod = productMapper.toDomain(productR);
@@ -97,8 +102,9 @@ public class OrderController extends BaseController<Order, OrderResource>{
         orderService.updateItem(ord,prod,quantity);
     }
     //@DeleteMapping(params = {"order","product"})
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     @RequestMapping("/items/remove")
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeItem(@RequestBody final OrderResource orderR, @RequestBody final ProductResource productR){
         var ord = orderMapper.toDomain(orderR);
         var prod = productMapper.toDomain(productR);
@@ -108,6 +114,7 @@ public class OrderController extends BaseController<Order, OrderResource>{
 
     //Finalize Order
 //    @PostMapping(params = {"order", "paymentMethod","address","orderNote"})
+    @PostMapping
     @RequestMapping("finalize")
     public ResponseEntity<ApiResponse<OrderResource>> finalizeOrder(
             @RequestBody final OrderResource orderR,
@@ -124,8 +131,29 @@ public class OrderController extends BaseController<Order, OrderResource>{
     }
 
 
+    //Get All orders of a user
+//    @GetMapping("user/{userId}")
+//    public ResponseEntity<ApiResponse<List<OrderResource>>> getOrdersByUser(@PathVariable("userId") final Long userId) {
+//        // Assuming orderService has a method to find orders by user
+//        List<Order> userOrders = orderService.findOrdersByUser(userId);
+//
+//        return ResponseEntity.ok(
+//                ApiResponse.<List<OrderResource>>builder()
+//                        .data(orderMapper.toResourceList(userOrders))
+//                        .build());
+//    }
 
+    //Get order by date
+    @GetMapping("date/{orderDate}")
+    public ResponseEntity<ApiResponse<List<OrderResource>>> getOrdersByDate(
+            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate orderDate) {
+        List<Order> ordersByDate = orderService.findOrdersByDate(orderDate);
 
+        return ResponseEntity.ok(
+                ApiResponse.<List<OrderResource>>builder()
+                        .data(orderMapper.toResourceList(ordersByDate))
+                        .build());
+    }
 
 
 
