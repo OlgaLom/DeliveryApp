7ee0761edf530aa8e8c972f32cb74ca1c8c0bd48
GitHub_AnastasiaBam <a.bampasi@gmail.com>
2024-01-25 12:14:00 +0200

    Added AddressMapper and AddressResource

---
+++ src/main/java/backend_group_5/we_lead_bootcamp/mapper/AddressMapper.java
@@ -0,0 +1,8 @@
+package backend_group_5.we_lead_bootcamp.mapper;
+
+import backend_group_5.we_lead_bootcamp.model.Address;
+import backend_group_5.we_lead_bootcamp.transfer.resource.AddressResource;
+import org.mapstruct.Mapper;
+@Mapper(componentModel = "spring")
+public interface AddressMapper extends BaseMapper<Address, AddressResource>{
+}
---
+++ src/main/java/backend_group_5/we_lead_bootcamp/transfer/resource/AddressResource.java
@@ -0,0 +1,14 @@
+package backend_group_5.we_lead_bootcamp.transfer.resource;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class AddressResource extends BaseResource{
+    private String address;
+    private Integer streetNumber;
+    private String city;
+}
