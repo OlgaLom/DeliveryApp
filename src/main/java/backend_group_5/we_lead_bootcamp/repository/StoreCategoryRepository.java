20bf5762c4ac51b8345a9bbe807085fc9c02cf29
olgaLom <olgamoumtzi@gmail.com>
2024-01-27 01:07:50 +0200

    Add query to the method findByName

--- src/main/java/backend_group_5/we_lead_bootcamp/repository/StoreCategoryRepository.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/repository/StoreCategoryRepository.java
@@ -3,10 +3,12 @@ package backend_group_5.we_lead_bootcamp.repository;
 import backend_group_5.we_lead_bootcamp.model.StoreCategory;
 import backend_group_5.we_lead_bootcamp.model.StoreCategoryVariation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface StoreCategoryRepository extends JpaRepository<StoreCategory, Long> {
+    @Query("SELECT st FROM StoreCategory st WHERE st.displayName = :name")
     Optional<StoreCategory> findByName(StoreCategoryVariation name);
 }
 
