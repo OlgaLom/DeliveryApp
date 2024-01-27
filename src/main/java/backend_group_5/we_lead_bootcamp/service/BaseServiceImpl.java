4033cafb0b8f79f81bc543a0480a1829aab06910
olgaLom <olgamoumtzi@gmail.com>
2024-01-27 21:06:59 +0200

    Add Transactional annotation

--- src/main/java/backend_group_5/we_lead_bootcamp/service/BaseServiceImpl.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/service/BaseServiceImpl.java
@@ -3,10 +3,14 @@ package backend_group_5.we_lead_bootcamp.service;
 import backend_group_5.we_lead_bootcamp.base.BaseComponent;
 import backend_group_5.we_lead_bootcamp.model.BaseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED, rollbackFor = Exception.class)
 public abstract class BaseServiceImpl<T extends BaseModel> extends BaseComponent implements BaseService<T,Long>{
 //    T stands for Type
     protected abstract JpaRepository<T, Long> getRepository();
@@ -17,8 +21,9 @@ public abstract class BaseServiceImpl<T extends BaseModel> extends BaseComponent
     @Override
     public List<T> createAll(final List<T> items){return getRepository().saveAll(items);}
 
+    @SafeVarargs
     @Override
-    public List<T> createAll(final T... items) {
+    public final List<T> createAll(final T... items) {
         return createAll(Arrays.asList(items));
     }
 
@@ -33,6 +38,7 @@ public abstract class BaseServiceImpl<T extends BaseModel> extends BaseComponent
     public void deleteById(final Long id){ getRepository().deleteById(id);}
 
     @Override
+    @Transactional(readOnly = true)
     public T getById(final Long id) {
 //        T  item = getRepository().findById(id).orElseThrow();
 //        return item;
@@ -40,12 +46,15 @@ public abstract class BaseServiceImpl<T extends BaseModel> extends BaseComponent
     }
 
     @Override
+    @Transactional(readOnly = true)
     public boolean exists(final T item){return getRepository().existsById(item.getId());}
 
     @Override
+    @Transactional(readOnly = true)
     public List<T> findAll() { return getRepository().findAll(); }
 
     @Override
+    @Transactional(readOnly = true)
     public Long count() { return getRepository().count();  }
 
 }
