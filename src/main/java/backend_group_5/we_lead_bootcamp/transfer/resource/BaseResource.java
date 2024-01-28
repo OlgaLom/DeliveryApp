bc8ed0fce39fd3477fb400379b28ba42ac984955
olgaLom <olgamoumtzi@gmail.com>
2024-01-29 01:02:10 +0200

    Add getter Setters and toString annotations

--- src/main/java/backend_group_5/we_lead_bootcamp/transfer/resource/BaseResource.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/transfer/resource/BaseResource.java
@@ -1,7 +1,14 @@
 package backend_group_5.we_lead_bootcamp.transfer.resource;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
+@ToString
 public class BaseResource implements Serializable {
     private Long id;
 }
