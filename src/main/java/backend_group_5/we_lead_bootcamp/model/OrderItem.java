479b3a3ac49a8e12cbbd1338e4612e8237323247
olgaLom <olgamoumtzi@gmail.com>
2024-01-28 13:33:04 +0200

    Change allocationSize to 1

--- src/main/java/backend_group_5/we_lead_bootcamp/model/OrderItem.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/model/OrderItem.java
@@ -15,7 +15,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Entity
 @Table(name="ORDER_ITEMS")
-@SequenceGenerator(name = "idGenerator", sequenceName = "ORDER_ITEMS_SEQ", initialValue = 1, allocationSize = 20)
+@SequenceGenerator(name = "idGenerator", sequenceName = "ORDER_ITEMS_SEQ", initialValue = 1, allocationSize = 1)
 public class OrderItem extends BaseModel {
     @ToString.Exclude
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
