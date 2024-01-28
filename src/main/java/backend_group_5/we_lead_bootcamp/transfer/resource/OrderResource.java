ddfc08684be805c2c7ac5bfc5eff394d4b3669a3
olgaLom <olgamoumtzi@gmail.com>
2024-01-29 01:03:49 +0200

    Add pattern to date.

--- src/main/java/backend_group_5/we_lead_bootcamp/transfer/resource/OrderResource.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/transfer/resource/OrderResource.java
@@ -1,7 +1,7 @@
 package backend_group_5.we_lead_bootcamp.transfer.resource;
 
 import backend_group_5.we_lead_bootcamp.model.Address;
-import backend_group_5.we_lead_bootcamp.model.OrderStatus;
+import backend_group_5.we_lead_bootcamp.model.enums.OrderStatus;
 import backend_group_5.we_lead_bootcamp.model.enums.PaymentMethod;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
@@ -9,6 +9,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -26,8 +27,10 @@ public class OrderResource extends BaseResource{
     private BigDecimal orderTotal;
     private String orderNumber;
     private String orderNote;
-    @JsonFormat(shape = JsonFormat.Shape.STRING)  // We declared in our application yml the default date format.
-    private Date createOrderDate;
+//    @JsonFormat(shape = JsonFormat.Shape.STRING)
+//    private Date createOrderDate;
+@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+private LocalDate createOrderDate;
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     private Date updateOrderDate;
 
