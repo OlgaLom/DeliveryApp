95748b68dbdde722d9c5c951d890ec3ce250198d
aimigogo <aimiliagogolou5@gmail.com>
2024-01-29 15:32:32 +0200

    Completed bootstrap for Products and ProductCategories

--- src/main/java/backend_group_5/we_lead_bootcamp/bootstrap/CatalogCustomerSampleContentCreator.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/bootstrap/CatalogCustomerSampleContentCreator.java
@@ -2,6 +2,7 @@ package backend_group_5.we_lead_bootcamp.bootstrap;
 
 import backend_group_5.we_lead_bootcamp.base.BaseComponent;
 import backend_group_5.we_lead_bootcamp.model.Product;
+import backend_group_5.we_lead_bootcamp.model.ProductCategory;
 import backend_group_5.we_lead_bootcamp.model.enums.Flavours;
 import backend_group_5.we_lead_bootcamp.model.enums.Sauces;
 import backend_group_5.we_lead_bootcamp.model.enums.Sizes;
@@ -16,6 +17,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.List;
 
 @Component
 @Profile("generate-catalog-customers")
@@ -29,119 +32,85 @@ public class CatalogCustomerSampleContentCreator extends BaseComponent implement
     @Override
     public void run(String... args) throws Exception {
         //create productCategory
-//       ProductCategory newCategory = productCategoryService.create(ProductCategory.builder()
-//               .description("Coffee")
-//               .name("Coffee House").build());
-//        logger.info("Created {}.", newCategory);
+        ProductCategory newCategory = productCategoryService.create(ProductCategory.builder()
+               .description("Rich Aromatic Coffee Blends")
+               .name("Cafe").build());
+        logger.info("Created {}.", newCategory);
+        //Createall productCategory
+        List<ProductCategory> productCategories= List.of(
+                ProductCategory.builder().description("Italian Pizza").name("Pizza").build(),
+                ProductCategory.builder().description("Fresh Artisanal Bread Varieties").name("Bakery").build(),
+                ProductCategory.builder().description("Delicious Morning Menu Options").name("Breakfast").build(),
+                ProductCategory.builder().description("Sizzling Grilled Flavor Creations").name("Grill").build(),
+                ProductCategory.builder().description("Delectable Sweet Treats Galore").name("Pastry").build(),
+                ProductCategory.builder().description("Juicy Gourmet Burger Creations").name("Burger").build());
 
-//        ProductCategory newCategory2=productCategoryService.create(ProductCategory.builder()
-//                .description("Pizza")
-//                .name("Pizzas")
-//                .build());
-//        logger.info("Created {}",newCategory2);
-//        ProductCategory newCategory3=productCategoryService.create(ProductCategory.builder()
-//                .description("Souvlaki,Giros,Steak")
-//                .name("Steakhouse")
-//                .build());
-//        logger.info("Created {}",newCategory3);
-
-//        //Create Store
-//        Store newStore = storeService.createStore(Store.builder()
-//                .name("Store1")
-//                .address("Address 1")
-//                .phone(String.valueOf("2310123453"))
-//                .vatNumber("VAT123")
-//                .minOrderAmount(BigDecimal.valueOf(5))
-//                .DeliveryTime(30).products(new ArrayList<>())
-//                .category(StoreCategoryVariation.CAFE)
-//                .build());
-//        logger.info("Created {}!",newStore);
-
-//        Store newStore2 = storeService.createStore(Store.builder()
-//                .name("Semeli")
-//                .address("Address 3")
-//                .phone(String.valueOf("2310123453"))
-//                .vatNumber("VAT124")
-//                .minOrderAmount(BigDecimal.valueOf(5))
-//                .DeliveryTime(30).products(new ArrayList<>())
-//                .category(StoreCategoryVariation.PIZZERIA)
-//                .build());
-//        logger.info("Created {}!",newStore2);
-
-//        Store newStore3 = storeService.createStore(Store.builder()
-//                .name("Babis")
-//                .address("Address 15")
-//                .phone(String.valueOf("2310123454"))
-//                .vatNumber("VAT128")
-//                .minOrderAmount(BigDecimal.valueOf(5))
-//                .DeliveryTime(30).products(new ArrayList<>())
-//                .category(StoreCategoryVariation.STEAKHOUSE)
-//                .build());
-//        logger.info("Created {}!",newStore3);
+        var productCategoriesCreated = productCategoryService.createAll(productCategories);
+        logger.info("Created {} product Categories.", productCategoriesCreated.size());
+        productCategoriesCreated.stream()
+                .sorted(Comparator.comparing(ProductCategory::getId))
+                .forEach(p -> logger.debug("{}. {}", p.getId(), p));
 
     //Create Product
-//        Product newProduct=productService.create(Product.builder()
-//                .name("Fredo Espresso")
-//                .price(BigDecimal.valueOf(4.30))
-//                .description("Hot espresso shaken with ice cubes")
-//                .productCategory(productCategoryService.getById(17L))
-//                .store(storeService.getStoreByName("Coffee House"))
-//                .serial("SN1000-0001")
-//                .sizes(Sizes.MEDIUM)
-//                .flavours(Flavours.HAZELNUT)
-//                .sauces(Sauces.TZATZIKI)
-//                .toppings(Toppings.WHITE_SUGAR)
-//                .build());
-//        logger.info("Created {}!",newProduct);
         Product newProduct=productService.create(Product.builder()
+                .serial("SN1000-0001")
                 .name("Fredo Espresso")
                 .price(BigDecimal.valueOf(4.30))
                 .description("Hot espresso shaken with ice cubes")
                 .productCategory(productCategoryService.getById(1L))
-                .store(storeService.getStoreByName("Helga Store"))
-                .serial("SN1000-0016")
-                .sizes(Sizes.NONE)
+                .store(storeService.getStoreByName("Store4"))
+                .sizes(Sizes.MEDIUM)
                 .flavours(Flavours.NONE)
                 .sauces(Sauces.NONE)
                 .toppings(Toppings.NONE)
                 .build());
         logger.info("Created {}!",newProduct);
 
+       //CreateAll Products
+       List<Product> products = List.of(
+                Product.builder().serial("SN1000-0002").name("HOT CHOCOLATE")
+                        .price(BigDecimal.valueOf(3.00)).productCategory(productCategoryService.getById(1L))
+                        .description("Classic Chocolate").store(storeService.getStoreByName("Sore4"))
+                        .sizes(Sizes.MEDIUM)
+                        .flavours(Flavours.STRAWBERRY)
+                        .toppings(Toppings.WHIPPED_CREAM)
+                        .sauces(Sauces.NONE)
+                        .build(),
+                Product.builder().serial("SN1100-0001").name("WAFFLE")
+                        .price(BigDecimal.valueOf(6.50)).productCategory(productCategoryService.getById(4L))
+                        .description("Waffle with Nutella").store(storeService.getStoreByName("Store9"))
+                        .sizes(Sizes.NONE)
+                        .flavours(Flavours.NONE)
+                        .sauces(Sauces.NONE)
+                        .toppings(Toppings.BISCUIT)
+                        .toppings(Toppings.WHIPPED_CREAM).build(),
+                Product.builder().serial("SN1100-0002").name("GREEK GIROS")
+                        .price(BigDecimal.valueOf(5.40)).productCategory(productCategoryService.getById(5L))
+                        .description("Greek Giros with fries").store(storeService.getStoreByName("Store3"))
+                        .sizes(Sizes.LARGE)
+                        .flavours(Flavours.NONE)
+                        .sauces(Sauces.TZATZIKI)
+                        .sauces(Sauces.KETCHUP)
+                        .sauces(Sauces.MUSTARD)
+                        .toppings(Toppings.NONE).build(),
+                Product.builder().serial("SN1100-0003").name("Burger")
+                        .price(BigDecimal.valueOf(8.00))
+                        .productCategory(productCategoryService.getById(7L))
+                        .store(storeService.getStoreByName("Store5"))
+                        .description("Best Burger in town!")
+                        .sizes(Sizes.LARGE)
+                        .flavours(Flavours.NONE)
+                        .sauces(Sauces.NONE)
+                        .toppings(Toppings.BACON)
+                        .build());
 
-//       List<Product> products = List.of(
-//                Product.builder().serial("SN1000-0002").name("HOT CHOCOLATE")
-//                        .price(BigDecimal.valueOf(3.00)).productCategory(productCategoryService.getById(17L))
-//                        .description("Classic Chocolate").store(storeService.getStoreByName("Coffee House"))
-//                        .sizes(Sizes.MEDIUM)
-//                        .flavours(Flavours.STRAWBERRY)
-//                        .toppings(Toppings.WHIPPED_CREAM)
-//                        .sauces(Sauces.NONE)
-//                        .build(),
-//                Product.builder().serial("SN1100-0001").name("WAFFLE")
-//                        .price(BigDecimal.valueOf(6.50)).productCategory(productCategoryService.getById(17L))
-//                        .description("Waffle with Nutella").store(storeService.getStoreByName("Coffee House"))
-//                        .toppings(Toppings.BISCUIT)
-//                        .toppings(Toppings.WHIPPED_CREAM).build(),
-//                Product.builder().serial("SN1100-0002").name("GREEK GIROS")
-//                        .price(BigDecimal.valueOf(1199)).productCategory(newCategory)
-//                        .description("Hot espresso shaken with ice cubes").store(newStore2)
-//                        .sizes(Sizes.LARGE).sauces(Sauces.TZATZIKI)
-//                        .sauces(Sauces.KETCHUP).sauces(Sauces.MUSTARD).build()
-//                Product.builder().serial("SN1100-0003").name("Pizza")
-//                        .price(BigDecimal.valueOf(15.00))
-//                        .productCategory(productCategoryService.getById(32L))
-//                        .store(storeService.getStoreByName("Semeli"))
-//                        .description("Best Pizza in town!").sizes(Sizes.LARGE)
-//                        .sauces(Sauces.NONE).flavours(Flavours.NONE)
-//                        .toppings(Toppings.BACON).toppings(Toppings.PEPERONI).build());
-//
-//                //ADD MORE PRODUCTS AS NEEDED
-//
-//        var productsCreated = productService.createAll(products);
-//        logger.info("Created {} products.", productsCreated.size());
-//        productsCreated.stream()
-//                .sorted(Comparator.comparing(Product::getId))
-//                .forEach(p -> logger.debug("{}. {}", p.getId(), p));
+                //ADD MORE PRODUCTS AS NEEDED
+
+        var productsCreated = productService.createAll(products);
+        logger.info("Created {} products.", productsCreated.size());
+        productsCreated.stream()
+                .sorted(Comparator.comparing(Product::getId))
+                .forEach(p -> logger.debug("{}. {}", p.getId(), p));
 //
 /*       List<User> customersCreated = customerService.createAll(
                 User.builder()
