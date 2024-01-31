c92d8e177a1b63ac146de7b782abb2672384b93d
aimigogo <aimiliagogolou5@gmail.com>
2024-01-31 02:05:02 +0200

    Small fixes in ProductController,ProductRepository and ProductResource

--- src/main/java/backend_group_5/we_lead_bootcamp/transfer/resource/ProductResource.java
+++ src/main/java/backend_group_5/we_lead_bootcamp/transfer/resource/ProductResource.java
@@ -1,6 +1,7 @@
 package backend_group_5.we_lead_bootcamp.transfer.resource;
 
 import backend_group_5.we_lead_bootcamp.model.ProductCategory;
+import backend_group_5.we_lead_bootcamp.model.Store;
 import backend_group_5.we_lead_bootcamp.model.enums.Flavours;
 import backend_group_5.we_lead_bootcamp.model.enums.Sauces;
 import backend_group_5.we_lead_bootcamp.model.enums.Sizes;
@@ -24,6 +25,8 @@ public class ProductResource extends BaseResource {
     private BigDecimal price;
     @NotNull
     private ProductCategory productCategory;
+    @NotNull
+    private Store store;
     private Sizes sizes;
     private Flavours flavours;
     private Sauces sauces;
